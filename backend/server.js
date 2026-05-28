7111ebc3d2a7f8c5eb8e97c229ede7620bb67f61
Mayur-Builds <khairnarm718@gmail.com>
2026-05-28 19:52:03 +0530

    created sign up and login api

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.send("HikeVerse Backend Running 🚀");
+});
+
+app.post("/signup", (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    console.log(username);
+    console.log(email);
+
+    res.send("Signup API Working 🚀");
+
+});
+
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    console.log(email);
+
+    res.send("Login API Working 🚀");
+
 });
 
 app.listen(PORT, () => {
