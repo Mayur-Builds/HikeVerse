a8507c445d588e1d42c48b79b16a5f7fcaa20073
Mayur-Builds <khairnarm718@gmail.com>
2026-05-30 10:43:04 +0530

    Completed mongodb signup and login api

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,77 @@
+const mongoose = require("mongoose");
 const express = require("express");
+const User = require("./models/User");
 
 const app = express();
 app.use(express.json());
 
+mongoose.connect(
+  "mongodb://mayur:MAYURMAYUR@ac-kaizhuq-shard-00-00.awwvtrw.mongodb.net:27017,ac-kaizhuq-shard-00-01.awwvtrw.mongodb.net:27017,ac-kaizhuq-shard-00-02.awwvtrw.mongodb.net:27017/?ssl=true&replicaSet=atlas-kfq1cx-shard-0&authSource=admin&appName=hikeverseDB"
+)
+.then(() => console.log("MongoDB Connected 🚀"))
+.catch((err) => {
+    console.log("MongoDB Error:");
+    console.error(err);
+});
 const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.send("HikeVerse Backend Running 🚀");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
 
-    const { username, email, password } = req.body;
+    try {
 
-    console.log(username);
-    console.log(email);
+        const { username, email, password } = req.body;
 
-    res.send("Signup API Working 🚀");
+        const newUser = new User({
+            username,
+            email,
+            password
+        });
+
+        await newUser.save();
+
+        res.send("User Registered Successfully 🚀");
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).send("Error");
+
+    }
 
 });
 
-app.post("/login", (req, res) => {
+    
 
-    const { email, password } = req.body;
 
-    console.log(email);
 
-    res.send("Login API Working 🚀");
+app.post("/login", async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.send("User Not Found ❌");
+        }
+
+        if (user.password !== password) {
+            return res.send("Invalid Password ❌");
+        }
+
+        res.send("Login Successful 🚀");
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).send("Error");
+
+    }
 
 });
 
