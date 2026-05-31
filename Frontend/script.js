c468fdc5ddb32fcd9597eba46b04d6fce67b7887
Mayur-Builds <khairnarm718@gmail.com>
2026-05-31 13:55:50 +0530

    Added trek search feature.

--- Frontend/script.js
+++ Frontend/script.js
@@ -112,3 +112,29 @@ async function loadTreks() {
         `;
     });
 }
+function searchTreks() {
+
+    const input =
+        document.getElementById("searchInput").value.toLowerCase();
+
+    const cards =
+        document.querySelectorAll(".trek-card");
+
+    cards.forEach((card) => {
+
+        const title =
+            card.querySelector("h2").innerText.toLowerCase();
+
+        if (title.includes(input)) {
+
+            card.style.display = "block";
+
+        } else {
+
+            card.style.display = "none";
+
+        }
+
+    });
+
+}
